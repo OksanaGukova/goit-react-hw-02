1a0fd72d72b965c2819d3370171e90e4cf8c412b
OksanaGukova <kseniagukova@ukr.net>
2024-06-11 19:55:49 +0300

    compotents

---
+++ src/components/App/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Feedback from '../Feedback'
+import Options from '../Options'
+
+export default function App() {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = (type) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+  };
+
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+      <Options onLeaveFeedback={handleFeedback} />
+      <Feedback feedback={feedback} />
+    </div>
+  );
+}
