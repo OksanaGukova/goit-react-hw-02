050f630396bcbc9d4c8652b495590f82d4cbed9d
OksanaGukova <kseniagukova@ukr.net>
2024-06-11 20:09:10 +0300

    task-1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Feedback from '../Feedback'
-import Options from '../Options'
+import  Feedback  from '../Feedback/Feedback'
+import  Options  from '../Options/Options'
 
 export default function App() {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,9 @@
-export default function Options() {
-    return (
-        <div>
-        <button>Good</button>
-    <button>Neutral</button>
-            <button>Bad</button>
-        </div>
-    )
-    
+export default function Options({ onLeaveFeedback }) {
+   return (
+     <div>
+       <button onClick={() => onLeaveFeedback("good")}>Good</button>
+       <button onClick={() => onLeaveFeedback("neutral")}>Neutral</button>
+       <button onClick={() => onLeaveFeedback("bad")}>Bad</button>
+     </div>
+   );
 }
